cbd49a22171936ac727068e376a09e65344511d2
OscarAndresVillamizarBonilla <andrex.villamizar1@gmail.com>
2023-02-16 19:06:51 -0500

    Primer punto

---
+++ condicionales_Edad_If_Else_13_02_2023/main.js
@@ -0,0 +1,6 @@
+let edad = Number(prompt("Vamos a validar si eres mayor de edad o no\n\t\tIngresa tu edad"));
+if (edad>=18) {
+    console.log(`Tu edad es de ${edad} años, es mayor de edad.`);
+} else {
+    console.log(`La edad ingresada no cumple con los parámetros.`);
+}
